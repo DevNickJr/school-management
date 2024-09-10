9dcbf665135543273e30a443f39febb076de950a
DevNickjr <nicholasduadei14@gmail.com>
2024-09-10 20:17:37 +0100

    update logo

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
 import AuthImage from '@/assets/auth.png'
-import Logo from '@/assets/logo.svg'
+import Logo from '@/assets/logo.png'
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
 
   return (
       <div className='flex w-full overflow-hidden md:h-screen'>
         <div className="relative flex-1 p-4 py-6 overflow-y-auto md:px-12">
-            <Image src={Logo} alt="Auth Image" className='h-12 mb-4' />
+            <Image src={Logo} alt="Auth Image" className='w-32 mb-4' />
             {children}
         </div>
         <div className="flex-1 hidden h-screen overflow-hidden md:flex">
